d8d194ed2213bb9db90a47d5236131dafc5abc3d
Samarjeet169 <samrjeetpowalkar@gmail.com>
2022-03-31 11:54:53 +0530

    yes

---
+++ SPringBootRunner/src/main/java/com/example/demo/Runner/EmailRUnner.java
@@ -0,0 +1,12 @@
+package com.example.demo.Runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+@Component
+public class EmailRUnner implements CommandLineRunner {
+@Override
+public void run(String... args) throws Exception {
+	// TODO Auto-generated method stub
+	System.out.println("Email Runner");
+}
+}
---
+++ SPringBootRunner/src/main/java/com/example/demo/SPringBootRunnerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SPringBootRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SPringBootRunnerApplication.class, args);
+	}
+
+}
